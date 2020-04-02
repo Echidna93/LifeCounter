799fe43fe79a32822fcaa4cc10de0db03d162301
Alexander Jack <jackx022@umn.edu>
2020-04-01 19:44:30 -0500

    add linear layout handles

--- app/src/main/java/com/example/lifecounter/MainActivity.java
+++ app/src/main/java/com/example/lifecounter/MainActivity.java
@@ -4,20 +4,24 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.text.Layout;
+import android.widget.LinearLayout;
 import android.widget.RelativeLayout;
 
 public class MainActivity extends AppCompatActivity {
-    private RelativeLayout layoutView1;
-    private RelativeLayout getLayoutView2;
+    private LinearLayout linearLayoutView1;
+    private LinearLayout linearLayoutView2;
+    private LinearLayout linearLayoutView3;
+    private LinearLayout linearLayoutView4;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
         // grab handles for relative layouts 1 and 2
-        //layoutView1 = (RelativeLayout) findViewById(R.id.layoutView1ID);
-        //getLayoutView2 = (RelativeLayout) findViewById(R.id.layoutView2ID);
-
+        linearLayoutView1 = (LinearLayout) findViewById(R.id.linearLayoutView1ID);
+        linearLayoutView2 = (LinearLayout) findViewById(R.id.linearLayoutView2ID);
+        linearLayoutView3 = (LinearLayout) findViewById(R.id.linearLayoutView3ID);
+        linearLayoutView4 = (LinearLayout) findViewById(R.id.linearLayoutView4ID);
 
 
     }
