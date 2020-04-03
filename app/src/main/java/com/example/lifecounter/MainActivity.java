d4ad1b872d9d42d66866c14de03a374d5d0bcddc
Alexander Jack <jackx022@umn.edu>
2020-04-03 17:29:12 -0500

    use materialdesign buttons, use materialdesign dark theme as default, update element id's

--- app/src/main/java/com/example/lifecounter/MainActivity.java
+++ app/src/main/java/com/example/lifecounter/MainActivity.java
@@ -15,8 +15,8 @@ public class MainActivity extends AppCompatActivity {
     //private LinearLayout linearLayoutView2;
     private LinearLayout linearLayoutView3;
     private TextView lifeCountTextView3;
-    private Button increaseLifeCountButton3;
-    private Button decreaseLifeCountButton3;
+    public Button increaseLifeCountButton3;
+    public Button decreaseLifeCountButton3;
     //private LinearLayout linearLayoutView4;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,14 +24,9 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         // grab handles for relative layouts 1 and 2
-
-        //linearLayoutView1 = (LinearLayout) findViewById(R.id.linearLayoutView1ID);
-        //linearLayoutView2 = (LinearLayout) findViewById(R.id.linearLayoutView2ID);
-        linearLayoutView3 = (LinearLayout) findViewById(R.id.linearLayoutView3ID);
-        lifeCountTextView3 = (TextView) findViewById(R.id.lifeCountViewID3);
-        increaseLifeCountButton3 = (Button) findViewById(R.id.increaseLifeCountButtonViewID3);
-        decreaseLifeCountButton3 = (Button) findViewById(R.id.decreaseLifeCountButtonViewID3);
-        //linearLayoutView4 = (LinearLayout) findViewById(R.id.linearLayoutView4ID);
+        lifeCountTextView3 = (TextView) findViewById(R.id.lifeCountView3ID);
+        increaseLifeCountButton3 = (Button) findViewById(R.id.increaseLifeCountButtonView3ID);
+        decreaseLifeCountButton3 = (Button) findViewById(R.id.decreaseLifeCountButtonView3ID);
 
         lifeCountTextView3.setText("40");
         increaseLifeCountButton3.setOnClickListener(new View.OnClickListener() {
